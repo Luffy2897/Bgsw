b74f111bc39134d270dea68f4a6ad8fad869c3df
Avinaash <avinaash897@gmail.com>
2023-09-14 17:33:02 +0530

    Commited

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+// import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+
+  templateUrl: './login.component.html',
+
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  // ngOnInit() {
+  //   history.pushState('', '', location.href);
+  //   window.onpopstate = function () {
+  //     history.go(1);
+  //   };
+  // }
+}
